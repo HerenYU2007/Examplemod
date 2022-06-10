32bdd3a5a6f595d4667ea32d3e473f9d9300e9f5
HerenYU <971054924yu@gmail.com>
2022-06-10 21:44:28 +0800

    Update Sherlock.java

--- src/main/java/net/fabricmc/example/item/Sherlock.java
+++ src/main/java/net/fabricmc/example/item/Sherlock.java
@@ -1,2 +1,20 @@
-package net.fabricmc.example.item;public class Sherlock {
+package net.fabricmc.example.item;
+
+import net.fabricmc.example.ExampleMod;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+public class Sherlock {
+
+    private static Item SHERLOCK = r("sherlock_dog",new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+
+    private static Item r(String name, Item item) {
+        return Registry.register(Registry.ITEM, new Identifier(ExampleMod.MOD_ID, name), item);
+    }
+    public static void register(){
+
+    }
 }
